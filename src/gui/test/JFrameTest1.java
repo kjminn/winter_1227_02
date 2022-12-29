24749a266e8ffd398202c5f679a0c1afa35d7742
kjminn <kjminn2001@gmail.com>
2022-12-29 14:14:50 +0900

    all

---
+++ src/gui/test/JFrameTest1.java
@@ -0,0 +1,22 @@
+package gui.test;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class JFrameTest1 {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		JLabel lbl = new JLabel("", JLabel.CENTER);
+		ImageIcon icon = new ImageIcon("images/sjk.jpg");
+		lbl.setIcon(icon);
+		frame.add(lbl);
+		frame.setTitle("JFrame 연습1");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocation(200, 200);
+		frame.setSize(500, 300);
+		frame.setVisible(true);
+	}
+
+}
